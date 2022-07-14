d2f9387a5b76246f8844dbc0e76bd54d77301eac
Punit9349 <puneetgarg9349@gmail.com>
2022-07-14 20:16:24 +0530

    commit

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 
 export default function Textform(props) {
 
+    const  [Text, setText] = useState("Enter your text here");
+
     const handleUpClick= ()=>{
       //  console.log("Uppercase was clicked" + Text);
         let newText= Text.toUpperCase();
@@ -14,13 +16,22 @@ export default function Textform(props) {
         setText(event.target.value);
       }
 
-    const handleloClick=(event)=>{
+    const handleloClick=()=>{
      // console.log("Lowecase was clicked");
         let newText= Text.toLowerCase();
         setText(newText);
     }  
 
-    const  [Text, setText] = useState("Enter your text here");
+    const handleclear=()=>{
+       //let newText=Text.clear();
+       setText(" ");
+    }
+
+    const removespaces=()=>{
+       let newText = Text.replace(/\s+/g,' ').trim();
+       setText(newText);
+    }
+
 
     return (
       <>
@@ -32,7 +43,8 @@ export default function Textform(props) {
             </div>
               <button type="button" className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
               <button className= "btn btn-primary" style={{marginLeft:9}} onClick={handleloClick}> Convert to Lowercase</button>
-              <button className= "btn btn-primary" style={{marginLeft:9}} onClick={handleloClick}> Clear Text</button>
+              <button className= "btn btn-primary" style={{marginLeft:9}} onClick={handleclear}> Clear Text</button>
+              <button className= "btn btn-primary" style={{marginLeft:9}} onClick={removespaces}> Remove Extra Spaces</button>
 
            
         
